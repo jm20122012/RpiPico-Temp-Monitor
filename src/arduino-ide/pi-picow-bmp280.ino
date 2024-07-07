57e7f56908a209c98d2481936cc3869e9ad5dc0a
jm20122012 <35246157+jm20122012@users.noreply.github.com>
2024-07-07 09:04:36 -0600

    Update pi-picow-bmp280.ino

--- src/arduino-ide/pi-picow-bmp280.ino
+++ src/arduino-ide/pi-picow-bmp280.ino
@@ -36,7 +36,7 @@ void setup() {
 
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
+    delay(100);
   }
 
   Serial.println("Starting setup...");
